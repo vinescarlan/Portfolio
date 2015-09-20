be74b1eeaeb791edf578184afedc64d6c78a0606
vinescarlan <vinescarlan@gmail.com>
2015-09-20 22:46:23 +0800

    Change offCanvas to nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var menuIcon = document.getElementById('hamburger');
-var offCanvas = document.getElementById('off-canvas');
+var nav = document.getElementsByTagName('nav')[0];
 
 menuIcon.addEventListener('click', function () {
-  if (offCanvas.className === undefined || offCanvas.className === "")
-  offCanvas.className = "menu-visible";
-  else offCanvas.className = "";
+  if (nav.className === undefined || nav.className === "")
+    nav.className = "menu-visible";
+  else nav.className = "";
 });
