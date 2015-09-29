d226fe0d64d4c0975d40046418dce1927fcd2796
vinescarlan <vinescarlan@gmail.com>
2015-09-29 15:02:06 +0800

    Create typing animation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,43 @@ var menuIcon = document.getElementById('hamburger');
 var body = document.getElementsByTagName('body')[0];
 
 menuIcon.addEventListener('click', function () {
-  if (body.className === undefined || body.className === "")
-    body.className = "menu-visible";
-  else body.className = "";
+	if (body.className === undefined || body.className === "")
+		body.className = "menu-visible";
+	else body.className = "";
 });
+
+var play = document.getElementById('play');
+var display = document.getElementById('display');
+
+function typingAnimation() {
+	// Wait for 1sec so the animation will run after the display opacity = 1
+	setTimeout(function () {
+		var message = "Hi! Thank you for spending time visiting my site. Hopefully, I can be a part of your team soon. Bye!";
+		var i = 0, blink = document.getElementById('blink');
+		var node = display.childNodes[1].firstChild;
+		
+		// Blinking underscore
+		setInterval(function () {
+			if (blink.innerHTML == "_") {
+				blink.innerHTML = " ";
+			} else {
+				blink.innerHTML = "_";
+			}
+		}, 500);
+		
+		// Add each character of 'message' to the display every 90ms
+		var animate = setInterval(function () {
+			node.innerHTML += message.charAt(i);
+			i++;
+			if (node.innerHTML == "$ " + message) {
+				clearInterval(animate);
+			}
+		}, 90);
+	}, 1000);
+}
+
+play.onclick = function () {
+	play.style.visibility = "hidden"; // Hide the play button
+	display.style.opacity = "1"; // Show the display
+	typingAnimation(); // Run animation
+};
