6b8ebed812731607f794fa604f68a57289408261
vinescarlan <vinescarlan@gmail.com>
2015-10-02 13:28:56 +0800

    Create addProjectItem function

--- js/main.js
+++ js/main.js
@@ -52,13 +52,89 @@
 	};
 })();
 
-var xhttp = new XMLHttpRequest();
+// Project items loader
+(function () {
+	function addProjectItem(project, num) {
+		// Prevent code repetition
+		function createElem(elem, className) {
+			var e = document.createElement(elem);
+			if (className !== undefined) e.className = className;
+			return e;
+		}
 
-xhttp.onreadystatechange = function () {
-	if (xhttp.readyState == 4 && xhttp.status == 200) {
-		var project = xhttp.responseText;
+		/**** DOM TREE of project items looks like this
+		 *  
+		 *  div#PROJECTS
+		 *  |
+		 *  |-- article.Project Item
+		 *      |           |
+		 *      |    section.mac-window -----------------------
+		 *      |           |                                 |
+		 *      |           div.top-part ----------    img.project-image
+		 *      |                 |               |
+		 *      |            div.lights      a.live-link
+		 *      |                 |
+		 *      |        span -- span -- span
+		 *      |
+		 *      |-- div.project-dfn ------------------
+		 *                 |             |           |
+		 *           h3.project-title    p   a.button-secondary
+		 *
+		 ***/
+
+		// Mac window
+		var projectItem = createElem("article", "project-item small-12 medium-6 columns");
+		var macWindow = createElem("section", "mac-window");
+		var topPart = createElem("div", "top-part");
+		var lights = createElem("div", "lights");
+		var website = createElem("a", "live-link");
+		website.href = "http://" + project[num].liveLink;
+		website.innerHTML = "view now &rarr;";
+		var projectImg = createElem("img", "project-image");
+		projectImg.src = project[num].img;
+
+		for (var i = 0, len = 3; i < len; i++) {
+			var bull = createElem("span");
+			bull.innerHTML = "&bull;";
+			bull.style.padding = "0 2px";
+			lights.appendChild(bull);
+		}
+		topPart.appendChild(lights);
+		topPart.appendChild(website);
+		macWindow.appendChild(topPart);
+		macWindow.appendChild(projectImg);
+		projectItem.appendChild(macWindow);
+
+		// Project definition
+		var projectDfn = createElem("div", "project-dfn");
+		var projectTitle = createElem("h3", "project-title");
+		projectTitle.innerHTML = project[num].title;
+		var p = createElem("p");
+		p.innerHTML = project[num].description + "<br/> Technologies Used: <span>" +
+			project[num].technologies.join(", ") + "</span>";
+		var source = createElem("a", "button-secondary");
+		source.href = "https://" + project[num].source;
+		source.innerHTML = "view source code";
+		projectDfn.appendChild(projectTitle);
+
+		projectDfn.appendChild(p);
+		projectDfn.appendChild(source);
+		projectItem.appendChild(projectDfn);
+
+		document.getElementById("projects").appendChild(projectItem);
 	}
-};
 
-xhttp.open("GET", "js/project_items.json", true);
-xhttp.send(null);
+	// Request project_items.JSON file
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function () {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var project = JSON.parse(xhttp.responseText);
+			for (var i = 0, len = project.length; i < len; i++)
+				addProjectItem(project, i);
+		}
+	};
+
+	xhttp.open("GET", "js/project_items.json", true);
+	xhttp.send(null);
+})();
